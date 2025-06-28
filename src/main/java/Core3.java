db9c28e362b2ea53be0e12329290b90fd5b6cb08
seshu-7 <seshukamini@gmail.com>
2025-06-28 18:50:35 -0400

    for and while loop

--- src/main/java/Core3.java
+++ src/main/java/Core3.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Core3 {
     public static void main(String[] args){
-
+        //Switch Cases
+         Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the day code: ");
+//        int day = sc.nextInt();
+//        switch(day){
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            default:
+//                System.out.println("wed - Sun");
+//        }
+        //For loop
+//        System.out.println("Enter the i: ");
+//        for(int i=sc.nextInt(); i<=100; i++){
+//            System.out.println(i);
+//        }
+        //while
+        System.out.println("Enter the j");
+        int j= sc.nextInt();
+        while(j>=1){
+            System.out.println(j);
+            j--;
+        }
     }
 }
