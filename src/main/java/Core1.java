9318abcf8fc19ca0a7507c814e12f63a43467259
seshu-7 <seshukamini@gmail.com>
2025-06-28 09:58:51 -0400

    Basic String Concepts executed.

--- src/main/java/Core1.java
+++ src/main/java/Core1.java
@@ -1,7 +1,25 @@
 public class Core1 {
     public static void main(String[] args){
-        int a=10,b=4;
-        int x=a+b, y=a-b, z=a*b, k=a/b, q=a%b;
-        System.out.println("x="+x+ " y="+y+ " z="+z+ " k="+k+ " q="+q );
+//        int a=10,b=4;
+//        int x=a+b, y=a-b, z=a*b, k=a/b, q=a%b;
+//        System.out.println("x="+x+ " y="+y+ " z="+z+ " k="+k+ " q="+q );
+
+        //STRINGS
+        String name1="Seshadri", name2="Kamini";
+        System.out.println("Length  "+name1.length()+" "+ name2.length());
+        System.out.println("getting character  "+name1.charAt(4)+" "+name2.charAt(3));
+        System.out.println("Concatenation  "+name1+name2);
+        System.out.println("Replace character in a string  "+name1.replace('i','y'));
+        System.out.println("Substring  "+name2.substring(2,6));
+
+        String n1= new String("Tata punch");
+        String n2=new String("Tata punch");
+        if(n1 == n2){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+
     }
 }
