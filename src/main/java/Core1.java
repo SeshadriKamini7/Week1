70f5df41457947a36e1e9ccfa33435a1ab7ed8e9
seshu-7 <seshukamini@gmail.com>
2025-06-28 09:28:48 -0400

    used basic operators and varibles

---
+++ src/main/java/Core1.java
@@ -0,0 +1,7 @@
+public class Core1 {
+    public static void main(String[] args){
+        int a=10,b=4;
+        int x=a+b, y=a-b, z=a*b, k=a/b, q=a%b;
+        System.out.println("x="+x+ " y="+y+ " z="+z+ " k="+k+ " q="+q );
+    }
+}
