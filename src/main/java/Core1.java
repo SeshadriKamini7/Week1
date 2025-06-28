3d06941b5e1b5662e5a9f90c4023536c8be1c162
seshu-7 <seshukamini@gmail.com>
2025-06-28 10:08:37 -0400

    Basic String Concepts executed with memory location explanation.

--- src/main/java/Core1.java
+++ src/main/java/Core1.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Core1 {
     public static void main(String[] args){
 //        int a=10,b=4;
@@ -5,21 +7,31 @@ public class Core1 {
 //        System.out.println("x="+x+ " y="+y+ " z="+z+ " k="+k+ " q="+q );
 
         //STRINGS
-        String name1="Seshadri", name2="Kamini";
-        System.out.println("Length  "+name1.length()+" "+ name2.length());
-        System.out.println("getting character  "+name1.charAt(4)+" "+name2.charAt(3));
-        System.out.println("Concatenation  "+name1+name2);
-        System.out.println("Replace character in a string  "+name1.replace('i','y'));
-        System.out.println("Substring  "+name2.substring(2,6));
+//        String name1="Seshadri", name2="Kamini";
+//        System.out.println("Length  "+name1.length()+" "+ name2.length());
+//        System.out.println("getting character  "+name1.charAt(4)+" "+name2.charAt(3));
+//        System.out.println("Concatenation  "+name1+name2);
+//        System.out.println("Replace character in a string  "+name1.replace('i','y'));
+//        System.out.println("Substring  "+name2.substring(2,6));
+//
+         // Compared Strings with different memory location
+//        String n1= new String("Tata punch");
+//        String n2=new String("Tata punch");
+//        if(n1 == n2){
+//            System.out.println("True");
+//        }
+//        else{
+//            System.out.println("False");
+//        }
 
-        String n1= new String("Tata punch");
-        String n2=new String("Tata punch");
-        if(n1 == n2){
+        // Compared Content even with different memory
+        String n3= new String("Audi Q5");
+        String n4=new String("Audi Q5");
+        if(n3.equals(n4)){
             System.out.println("True");
         }
         else{
             System.out.println("False");
         }
-
     }
 }
