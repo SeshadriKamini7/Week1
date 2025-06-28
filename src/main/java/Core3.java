b140fc805e390201c5ddcd4c98a905101a65bc67
seshu-7 <seshukamini@gmail.com>
2025-06-28 19:18:19 -0400

    break and continue

--- src/main/java/Core3.java
+++ src/main/java/Core3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Core3 {
     public static void main(String[] args){
         //Switch Cases
-         Scanner sc = new Scanner(System.in);
+//         Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter the day code: ");
 //        int day = sc.nextInt();
 //        switch(day){
@@ -22,11 +22,26 @@ public class Core3 {
 //            System.out.println(i);
 //        }
         //while
-        System.out.println("Enter the j");
-        int j= sc.nextInt();
-        while(j>=1){
-            System.out.println(j);
-            j--;
-        }
+//        System.out.println("Enter the j");
+//        int j= sc.nextInt();
+//        while(j>=1){
+//            System.out.println(j);
+//            j--;
+//        }
+
+        //Continue Break Statements
+//        int i=0;
+//        while(true) {
+//            if (i == 3) {
+//                i++;
+//                continue;
+//            }
+//            System.out.println(i);
+//            i++;
+//            if (i > 5) {
+//                break;
+//            }
+//        }
+
     }
 }
