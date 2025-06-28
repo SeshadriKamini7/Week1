81bfc7f137e7174bb459a0c4bb86e333102d0207
seshu-7 <seshukamini@gmail.com>
2025-06-28 11:05:22 -0400

    1D and 2D learned

--- src/main/java/Core2.java
+++ src/main/java/Core2.java
@@ -2,14 +2,24 @@ import java.util.Arrays;
 
 public class Core2 {
     public static void main(String[] args){
-        int[] marks = new int[4];
-        marks[0]=94;
-        marks[1]=45;
-        marks[2]=65;
-        marks[3]=86;
-        System.out.println(marks.length);
-        System.out.println(marks[2]);
-        Arrays.sort(marks);
-        System.out.println(marks[2]);
+//        int[] marks = new int[4];
+//        marks[0]=94;
+//        marks[1]=45;
+//        marks[2]=65;
+//        marks[3]=86;
+//        System.out.println(marks.length);
+//        System.out.println(marks[2]);
+//        Arrays.sort(marks);
+//        System.out.println(marks[2]);
+
+        //1D Array
+//        int[] marks1 = {23,56,62,51};
+//        System.out.println(marks1[2]);
+//        Arrays.sort(marks1);
+//        System.out.println(marks1[2]);
+
+        //2D Array
+//        int[][] Movie_Coll ={{200, 100, 1500, 400},{250,850,730,400}};
+//        System.out.println(Movie_Coll[0][1] +" "+ Movie_Coll[1][3]);
     }
 }
