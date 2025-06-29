708735a6dbd53acb7e1592995fe0de7e643d9bd2
seshu-7 <seshukamini@gmail.com>
2025-06-29 08:24:00 -0400

    class, object, functions

---
+++ src/main/java/ObjOP/Oops.java
@@ -0,0 +1,30 @@
+package ObjOP;
+//Creating class,object and functions
+//class Pen{
+//    String color;
+//    String type;
+//    void write(){
+//        System.out.println("Khansaar ka Salaar");
+//    }
+//    void Dispcolor(){
+//        System.out.println(this.color);
+//    }
+//    void Disptype(){
+//        System.out.println(this.type);
+//    }
+
+//}
+public class Oops {
+public static void main(String[] args){
+//    Pen pen1 = new Pen();
+//    Pen pen2 = new Pen();
+//
+//    pen1.color = "Blue";
+//    pen1.type = "Gel";
+//    pen2.color = "Black";
+//    pen2.type ="Ballpoint";
+//    pen1.write();
+//    pen2.Disptype();
+
+}
+}
