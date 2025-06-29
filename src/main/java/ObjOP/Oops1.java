f332a5fdc83c2a35ccd62b363bfd212b7b5e51ed
seshu-7 <seshukamini@gmail.com>
2025-06-29 10:04:15 -0400

    polymorphism

--- src/main/java/ObjOP/Oops1.java
+++ src/main/java/ObjOP/Oops1.java
@@ -17,6 +17,29 @@ package ObjOP;
 //        System.out.println(name+" "+age);
 //    }
 //}
+
+//Inheritance
+//class shape{
+//    void area(){
+//        System.out.println("Displaying the area");
+//    }
+//}
+//class triangle extends shape{
+//    void area(int l, int b){
+//        System.out.println(1/2*l*b);
+//    }
+//}
+//class eqtriangle extends triangle{
+//    void area(int l, int b){
+//        System.out.println(1/2*l*b);
+//    }
+//}
+//class circle extends shape{
+//    int r;
+//    void area(int r){
+//        System.out.println(3.14*r*r);
+//    }
+//}
 public class Oops1 {
     public static void main(String[] args){
         //Polymorphism
@@ -25,6 +48,11 @@ public class Oops1 {
 //        s1.age=23;
 //        s1.printinfo(s1.name, s1.age);
 //        s1.printinfo(s1.age);
+
+        //Inheritance
+//        circle c1 = new circle();
+//        c1.r=7;
+//        c1.area(c1.r);
     }
 }
 
