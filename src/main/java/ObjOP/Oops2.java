549d1e7bf88c863388afe0a75b839410c28c4f57
seshu-7 <seshukamini@gmail.com>
2025-06-29 19:28:04 -0400

    -

--- src/main/java/ObjOP/Oops2.java
+++ src/main/java/ObjOP/Oops2.java
@@ -1,6 +1,7 @@
 package ObjOP;
 import java.util.Random;
-//class Account{
+//Encapsulation
+// class Account{
 //    String name;
 //    protected String gmail;
 //    private String password;
@@ -12,13 +13,42 @@ import java.util.Random;
 //        this.password=pass;
 //    }
 //}
+
+//Abstraction
+//abstract class Animal{
+//    abstract void walk();
+//    Animal(){
+//        System.out.println("you created a new animal");
+//    }
+//    void eat(){
+//        System.out.println("Animal eat food");
+//    }
+//}
+//class horse extends Animal{
+//    horse(){
+//        System.out.println("Created a Horse");
+//    }
+//    void walk(){
+//        System.out.println("This animal walks on 4 legs");
+//    }
+//}
+//class hen extends Animal{
+//    void walk(){
+//        System.out.println("This Animal walks on 2 legs");
+//    }
+//}
 public class Oops2 {
     public static void main(String[] args){
+        //Encapsulation
 //        Account s1 = new Account();
 //        s1.name="Seshadri";
 //        s1.gmail="sk@gmail.com";
 //                          s1.setPassword("abcd");
 //        System.out.println(s1.getPassword());
+
+        //Abstraction
+//        horse Horse =new horse();
+//        hen Hen = new hen();
 
     }
 }
