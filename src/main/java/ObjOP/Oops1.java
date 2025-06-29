8e9c4558e44a7c0d2f89b4171ef87593c5ef7505
seshu-7 <seshukamini@gmail.com>
2025-06-29 09:33:12 -0400

    polymorphism

---
+++ src/main/java/ObjOP/Oops1.java
@@ -0,0 +1,30 @@
+package ObjOP;
+//Polymorphism
+//class Student{
+//    int age;
+//    String name;
+//    void printinfo(String name){
+//        this.name=name;
+//        System.out.println(name);
+//    }
+//    void printinfo(int age){
+//        this.age=age;
+//        System.out.println(age);
+//    }
+//    void printinfo(String name,int age){
+//        this.age=age;
+//        this.name=name;
+//        System.out.println(name+" "+age);
+//    }
+//}
+public class Oops1 {
+    public static void main(String[] args){
+        //Polymorphism
+//        Student s1 =new Student();
+//        s1.name="Seshadri";
+//        s1.age=23;
+//        s1.printinfo(s1.name, s1.age);
+//        s1.printinfo(s1.age);
+    }
+}
+
