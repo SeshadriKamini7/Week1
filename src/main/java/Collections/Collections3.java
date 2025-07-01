c4c93afcbdd65a5cbd3c53d528e0851349532455
seshu-7 <seshukamini@gmail.com>
2025-07-01 06:44:00 -0400

    TreeMap, HashMap

---
+++ src/main/java/Collections/Collections3.java
@@ -0,0 +1,46 @@
+package Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Collections3 {
+    public static void main(String[] args) {
+        //HashMap
+//        Map<String, Integer> numbers = new HashMap<>();
+//        numbers.put("One", 1);
+//        numbers.put("Two", 2);
+//        numbers.put("Three", 3);
+//        System.out.println(numbers);
+
+//        if(!numbers.containsKey("Two")){
+//            numbers.put("Two", 23);
+//        numbers.putIfAbsent("Two", 23);
+//        System.out.println(numbers.containsKey("Two"));
+//        System.out.println(numbers.containsValue(5));
+//        System.out.println(!numbers.isEmpty());
+//        for(Map.Entry<String, Integer> e : numbers.entrySet()){
+//            System.out.println(e);
+//            System.out.println(e.getKey());
+//            System.out.println(e.getValue());
+//        }
+//        for(String key : numbers.keySet()){
+//            System.out.println(key);
+//        }
+//        for(Integer value : numbers.values()){
+//            System.out.println(value);
+//   }
+
+        //TreeMap
+//        Map<String, Integer> numbu = new TreeMap<>();
+//        numbu.put("One", 1);
+//        numbu.put("Two", 2);
+//        numbu.put("Three", 3);
+//        numbu.put("Four", 4);
+//        numbu.put("Five", 5);
+//        System.out.println(numbu);
+//        numbu.remove("Four");
+//        System.out.println(numbu);
+
+    }
+}
