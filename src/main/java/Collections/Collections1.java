99b17d73a50e0560f6b9d12729dc9d3150658aa4
seshu-7 <seshukamini@gmail.com>
2025-06-30 21:03:50 -0400

    Arraylist, Set, Iterator,Stack, QUeue and linkedlist

---
+++ src/main/java/Collections/Collections1.java
@@ -0,0 +1,63 @@
+package Collections;
+
+import java.util.*;
+
+public class Collections1 {
+    public static void main(String[] args){
+//        ArrayList<Integer> box = new ArrayList<>(3);
+//        box.add(2);
+//        box.add(4);
+//        box.add(0,43);
+//        box.add(31);
+//        box.add(21);
+//        System.out.println(box);
+
+        //ArrayList
+//        ArrayList<Integer> box2 = new ArrayList<>();
+//        box2.add(45);
+//        box2.add(54);
+//        box2.add(0, 61);
+//        System.out.println(box2);
+//
+//        box.addAll(box2);
+//        System.out.println(box);
+//
+//        box.remove(3);
+//        System.out.println(box);
+//         box.remove(Integer.valueOf(21));
+//        System.out.println(box);
+
+        //Set
+//        box.set(2, 20);
+//        System.out.println(box);
+//
+//        System.out.println(box.contains(20));
+
+        //Iterator
+//        Iterator<Integer> it = box.iterator();
+//            while(it.hasNext()){
+//                System.out.println("iterator"+ it.next());
+//            }
+
+        //Stack
+//        Stack<String> animal = new Stack<>();
+//        animal.push("horse");
+//        animal.push("cat");
+//        animal.push("Lion");
+//        System.out.println(animal);
+//
+//        System.out.println(animal.peek());
+//        animal.pop();
+//        System.out.println(animal);
+
+        //Queue and linkedlist
+//        Queue<Integer> box = new LinkedList<>();
+//        box.offer(32);
+//        box.offer(23);
+//        box.offer(41);
+//        System.out.println(box);
+//        System.out.println(box.peek());
+//        box.poll();
+//        System.out.println(box);
+    }
+}
