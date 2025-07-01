0aae1ae889313ea50310210bff1c0f2663f2ab8a
seshu-7 <seshukamini@gmail.com>
2025-06-30 21:43:58 -0400

    Priority Queue, Hashset and Arraydeque

---
+++ src/main/java/Collections/Collections2.java
@@ -0,0 +1,50 @@
+package Collections;
+import java.util.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Collections2 {
+    public static void main(String[] args) {
+        //Priority Queue
+//        Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+//        pq.offer(32);
+//        pq.offer(23);
+//        pq.offer(41);
+//        System.out.println(pq);
+//        System.out.println(pq.peek());
+//        System.out.println(pq.poll());
+//        System.out.println(pq);
+
+        //Arraydeque
+//        ArrayDeque<Integer> adq = new ArrayDeque<>();
+//        adq.offer(23);
+//        adq.offerFirst(41);
+//        adq.offer(32);
+//        adq.offerLast(12);
+//        adq.offer(11);
+//        System.out.println(adq);
+//        System.out.println(adq.peek());
+//        System.out.println(adq.peekLast());
+//        adq.pop();
+//        System.out.println(adq);
+//        adq.poll();
+//        System.out.println(adq);
+//        adq.pollLast();
+//        System.out.println(adq);
+//        System.out.println(adq.poll());
+
+        //Hashset
+//        Set<Integer> adq = new HashSet<>();
+//        adq.add(23);
+//        adq.add(41);
+//        adq.add(32);
+//        adq.add(12);
+//        adq.add(11);
+//        System.out.println(adq);
+//        adq.remove(23);
+//        System.out.println(adq);
+//        System.out.println(adq.contains(33));
+//        System.out.println(adq.isEmpty());
+//        System.out.println(adq.size());
+    }
+}
